b6813d7d31cd496ce8092e8e9872e71ff7ed2571
Max Bridges <mbridges@redhat.com>
2023-02-13 13:26:59 -0500

    Additional puttering

--- conditionals_script.py
+++ conditionals_script.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import os
 import re
-from itertools import chain
 
 # TODO: Modify for CLI use later.
 # project_path = os.path.abspath(os.curdir)
@@ -17,6 +16,7 @@ def find_ifevals_in_module(adoc_content):
     ifeval_re = re.compile(r'ifeval::\["{context}"\s*==\s*".*"\](?:\n|.)*?endif::\[\]')
     return re.findall(ifeval_re, adoc_content)
 
+
 def get_contexts_from_ifeval(ifeval):
     context_re = re.compile(r'\["{context}"\s==\s"([a-z0-9-]+)"\]')
     contexts = re.findall(context_re, ifeval)
@@ -29,7 +29,7 @@ def get_attrs_from_ifeval(ifeval):
     return attributes or None
 
 
-def compare_contexts(contexts):
+def compare_contexts(module, contexts):
     if not contexts:
         return
     context_dict = {}
@@ -37,9 +37,10 @@ def compare_contexts(contexts):
         context_dict[context] = context_dict.get(context, 0) + 1
     for context, count in context_dict.items():
         if count % 2 != 0:
-            print(f"Mismatch detected for context {context}")
+            print(f"Mismatch detected in {module} for context '{context}'")
 
-def compare_attrs(attrs):
+
+def compare_attrs(module, attrs):
     if not attrs:
         return
     attr_dict = {}
@@ -51,34 +52,29 @@ def compare_attrs(attrs):
             attr_dict[attr] = attr_dict.get(attr, 0) + 1
     for attr, count in attr_dict.items():
         if count != 0:
-            print(f"Mismatch detected for attribute {attr}")
+            print(f"Mismatch detected in {module} for attribute '{attr}'")
 
 
 def main():
     module_collection = collect_modules(modules_path)
 
     for module in module_collection:
+        print(f"Scanning {module}")
         mod = os.path.join(modules_path, module)
         contexts = []
         attrs = []
         with open(mod, "r") as file:
-            print(f"Scanning {mod}")
             module_content = file.read()
             for ifeval in find_ifevals_in_module(module_content):
-                contexts.append(get_contexts_from_ifeval(ifeval))
-                attrs.append(get_attrs_from_ifeval(ifeval))
-        try:
-            contexts = list(chain.from_iterable(contexts))
-            attrs = list(chain.from_iterable(attrs))
-            if contexts is not None or attrs is not None:
-                compare_contexts(contexts)
-                compare_attrs(attrs)
-        except:
-            # Try block is bad at the moment. Improve. Too many NoneTypes that I'm not dealing with in compare functions.
-            print(f"NoneType error in {mod}")
+                context = get_contexts_from_ifeval(ifeval)
+                if context is not None:
+                    contexts += context
 
-
-
+                attr = get_attrs_from_ifeval(ifeval)
+                if attr is not None:
+                    attrs += attr
+        compare_contexts(module, contexts)
+        compare_attrs(module, attrs)
 
 
 if __name__ == "__main__":
